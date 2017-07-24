a16319858f3c28e117fc7a9e4c34278d92507ac0
ar--beid <ar.beid@xs4all.nl>
2017-07-24 10:58:48 +0200

    update bats

--- example/run-open.bat
+++ example/run-open.bat
@@ -1,10 +1,10 @@
 @echo off
 
-SET URL=https://red-interviewing.niposoftware-dev.com/Interviews/cGvxV/hTqSe04AucXfrNM7hLnS
+SET URL=https://red-interviewing.niposoftware-dev.com/Interviews/cGvxV/pTAStRcpQOU1iXjJ7dlR
 
-SET INTERVIEWS=C:\wrk\run-interviews\example\open-i.csv
-SET QNAIRE=C:\wrk\run-interviews\example\open-q.csv
+SET INTERVIEWS=C:\wrk\ar-beid\run-interviews\example\open-i.csv
+SET QNAIRE=C:\wrk\ar-beid\run-interviews\example\open-q.csv
 
-SET EXE=C:\wrk\run-interviews\Runner\bin\Debug\runner.exe
+SET EXE=C:\wrk\ar-beid\run-interviews\Runner\bin\Debug\runner.exe
 
 call %EXE% "%URL%" "%QNAIRE%" "%INTERVIEWS%"
--- example/run.bat
+++ example/run.bat
@@ -2,10 +2,9 @@
 
 SET URL=https://red-interviewing.niposoftware-dev.com/Interviews/cGvxV/qky74PF8JHAg3WRT8EK8
 
-SET INTERVIEWS=C:\wrk\run-interviews\example\reporting-test-interviews-12.csv
-SET QNAIRE=C:\wrk\run-interviews\example\reporting-test-questionnaire.csv
+SET INTERVIEWS=C:\wrk\ar-beid\run-interviews\example\reporting-test-interviews-1.csv
+SET QNAIRE=C:\wrk\ar-beid\run-interviews\example\reporting-test-questionnaire.csv
 
-SET EXE=C:\wrk\run-interviews\Runner\bin\Debug\runner.exe
-
+SET EXE=C:\wrk\ar-beid\run-interviews\Runner\bin\Debug\runner.exe
 
 call %EXE% "%URL%" "%QNAIRE%" "%INTERVIEWS%"
