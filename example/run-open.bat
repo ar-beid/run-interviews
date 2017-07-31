d13a2f38d9fb05bc702f3b30ad59346dd76d963d
ar--beid <ar.beid@xs4all.nl>
2017-07-31 13:39:23 +0200

    test

--- example/run-open.bat
+++ example/run-open.bat
@@ -1,6 +1,6 @@
 @echo off
 
-SET URL=https://red-interviewing.niposoftware-dev.com/Interviews/cGvxV/pTAStRcpQOU1iXjJ7dlR
+SET URL=https://red-interviewing.niposoftware-dev.com/Interviews/cGvxV/mBPtyFF1C7jLqxeG8hRV
 
 SET INTERVIEWS=C:\wrk\ar-beid\run-interviews\example\open-i.csv
 SET QNAIRE=C:\wrk\ar-beid\run-interviews\example\open-q.csv
--- example/run.bat
+++ example/run.bat
@@ -1,8 +1,8 @@
 @echo off
 
-SET URL=https://red-interviewing.niposoftware-dev.com/Interviews/cGvxV/faNvPcEjj5Ax8sppX6oH
+SET URL=https://red-interviewing.niposoftware-dev.com/Interviews/cGvxV/MMiaGOJwxo8qnVhmBUMd
 
-SET INTERVIEWS=C:\wrk\ar-beid\run-interviews\example\reporting-test-interviews-test.csv
+SET INTERVIEWS=C:\wrk\ar-beid\run-interviews\example\reporting-test-interviews-exit.csv
 SET QNAIRE=C:\wrk\ar-beid\run-interviews\example\reporting-test-questionnaire.csv
 
 SET EXE=C:\wrk\ar-beid\run-interviews\Runner\bin\Debug\runner.exe
