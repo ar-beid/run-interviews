5d5065cfc5114ca8e323daeab2e8e18caf37431a
arjen jonker <a.jonker@niposoftware.com>
2017-08-09 20:46:37 +0200

    initial version

---
+++ posh/find-missing-entries.ps1
@@ -0,0 +1,24 @@
+# args[0] this file misses some entries that the 
+# args[1] file has; print these to the console 
+ 
+$input1 = Get-Content $args[0] 
+$input2 = Get-Content $args[1] 
+ 
+foreach ($i2 in $input2) 
+{ 
+  foreach ($i1 in $input1) 
+  { 
+    $match = 0 
+ 
+    if ($i1 -eq $i2)  
+    { 
+      $match = 1 
+      break 
+    } 
+  } 
+  
+  if (!($match)) 
+  { 
+    $i2 
+  } 
+} 
