a5066e7be2b5b20703ee42a15fa7a02f12559b9a
arjonker <a.jonker@niposoftware.com>
2017-08-10 14:50:27 +0200

    update paths

--- interviews/example/run.bat
+++ interviews/example/run.bat
@@ -1,10 +1,10 @@
 @echo off
 
-SET URL=https://rc-interviewing.niposoftware-dev.com/Interviews/J3qR1/x3MUyyEdB8KyJ78GErjC
+SET URL=https://red-interviewing.niposoftware-dev.com/Interviews/amd0U/gTKPjgt7QeMGOkzDjn4H
 
-SET INTERVIEWS=C:\wrk\ar-beid\run-interviews\example\reporting-test-interviews-100+.csv
-SET QNAIRE=C:\wrk\ar-beid\run-interviews\example\reporting-test-questionnaire.csv
+SET INTERVIEWS=C:\wrk\ar-beid\work\interviews\example\reporting-test-interviews-exit.csv
+SET QNAIRE=C:\wrk\ar-beid\work\interviews\example\reporting-test-questionnaire.csv
 
-SET EXE=C:\wrk\ar-beid\run-interviews\Runner\bin\Debug\runner.exe
+SET EXE=C:\wrk\ar-beid\work\interviews\Runner\bin\Debug\runner.exe
 
 call %EXE% "%URL%" "%QNAIRE%" "%INTERVIEWS%"
