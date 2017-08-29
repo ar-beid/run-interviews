183f40cb90ab01b97ffb2123b7b95576ee642672
arjonker <a.jonker@niposoftware.com>
2017-08-29 08:53:12 +0200

    update

--- interviews/example/run.bat
+++ interviews/example/run.bat
@@ -1,8 +1,8 @@
 @echo off
 
-SET URL=https://red-interviewing.niposoftware-dev.com/Interviews/amd0U/gTKPjgt7QeMGOkzDjn4H
+SET URL=https://red-interviewing.niposoftware-dev.com/Interviews/m2IXp/8rQxb2gBLd8ZCjjnGaUx
 
-SET INTERVIEWS=C:\wrk\ar-beid\work\interviews\example\reporting-test-interviews-exit.csv
+SET INTERVIEWS=C:\wrk\ar-beid\work\interviews\example\reporting-test-interviews-100+.csv
 SET QNAIRE=C:\wrk\ar-beid\work\interviews\example\reporting-test-questionnaire.csv
 
 SET EXE=C:\wrk\ar-beid\work\interviews\Runner\bin\Debug\runner.exe
