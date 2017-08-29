580a485ff904c673efafe13c4f3f38cbf79937cb
arjonker <a.jonker@niposoftware.com>
2017-08-29 12:19:06 +0200

    add simple q

--- interviews/example/run-open.bat
+++ interviews/example/run-open.bat
@@ -2,9 +2,9 @@
 
 SET URL=https://interviewing.nfieldbeta.com/Interviews/Jwxn7/gzp3O4F8Iyw1fp0O2ns1
 
-SET INTERVIEWS=C:\wrk\ar-beid\run-interviews\example\open-i.csv
-SET QNAIRE=C:\wrk\ar-beid\run-interviews\example\open-q.csv
+SET INTERVIEWS=C:\wrk\ar-beid\work\interviews\example\open-i.csv
+SET QNAIRE=C:\wrk\ar-beid\work\interviews\example\open-q.csv
 
-SET EXE=C:\wrk\ar-beid\run-interviews\Runner\bin\Debug\runner.exe
+SET EXE=C:\wrk\ar-beid\work\interviews\Runner\bin\Debug\runner.exe
 
 call %EXE% "%URL%" "%QNAIRE%" "%INTERVIEWS%"
